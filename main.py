b13a616bc27dbbb86be7d8eb3687a9b552ff0fc4
Aditya Mishra <mishraaaditya29@gmail.com>
2025-12-09 16:06:38 +0530

    added GET endpoint and implemented POST endpoint for book creation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()  # Initialize FastAPI application
 
@@ -12,3 +14,27 @@ async def read_root():
 async def greet_name(name: str) -> dict:
     # Return a greeting message with the provided name
     return {"greeting": f"Hello, {name}!"}
+
+# adding query paraemeter with name and age
+
+
+@app.get('/greet/{username}')  # Defining a GET endpoint with path parameter
+async def greet_user(username: Optional[str] = "User", age: int = "0") -> dict:
+    # Return a greeting message with the provided username and age
+    return {"greeting": f"Hello, {username}! You are {age} years old."}
+
+
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
+
+
+# sending data to server with POST request
+# if we are sending data then we also need to validate it using serializers or pydantic models
+@app.post('create_book')
+# book_data is an instance of BookCreateModel which will contain the data sent by the client
+async def create_book(book_data: BookCreateModel):
+    return {
+        "title": book_data.title,
+        "author": book_data.author
+    }
