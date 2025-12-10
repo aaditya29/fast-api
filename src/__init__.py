ea4dbc6455651cbe0597216e5a9554aa63b0c8da
Aditya Mishra <mishraaaditya29@gmail.com>
2025-12-10 12:14:17 +0530

    added lifespan context manager to FastAPI app for startup and shutdown messages

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print("Now we are starting the server")
+    yield  # used to run code before and after the application runs
+    print("Server has stopped now.")
+
 
 version = "v1.0.0"
 
 app = FastAPI(
     title="Book Management API",
     description="A simple FastAPI application to manage a collection of books.",
-    version=version
+    version=version,
+    lifespan=life_span,
 )  # Initialize FastAPI app
 
 # Include book router
