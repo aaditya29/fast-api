2e44f0219cbb258a9dc1a3f40545b5d075698a80
Aditya Mishra <mishraaaditya29@gmail.com>
2025-12-10 12:32:33 +0530

    added init_db function to initialize the database

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
 from contextlib import asynccontextmanager
+from src.db.main import init_db
 
 
 @asynccontextmanager
 async def life_span(app: FastAPI):
     print("Now we are starting the server")
+    await init_db()  # initialize the database when the server starts
     yield  # used to run code before and after the application runs
     print("Server has stopped now.")
 
--- src/db/main.py
+++ src/db/main.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, text
 from sqlalchemy.ext.asyncio import AsyncEngine
 from src.config import Config
 
@@ -6,3 +6,11 @@ engine = AsyncEngine(
     # create the async engine using the database URL from config
     # echo = true means to log all the SQL statements
     create_engine(url=Config.DATABASE_URL, echo=True))
+
+
+async def init_db():  # defining async function to initialize the database
+    async with engine.begin() as conn:  # creating an async connection as conn
+        statement = text("SELECT 'hello world';")  # Fixed: added closing quote
+        # executing the statement asynchronously
+        result = await conn.execute(statement)
+        print(result.all())  # printing all results
